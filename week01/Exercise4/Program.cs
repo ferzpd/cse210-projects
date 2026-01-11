0aa66c95f05f8616048eb52b27d4d0719370046a
ferzpd <ferzpd25@hotmail.com>
2026-01-10 17:50:34 -0700

    lists and generic

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int input = -1;
+
+        Console.WriteLine("Enter a list of numbers type 0 when finished.");
+        while (input != 0)
+        {
+            Console.Write("Enter numbeer: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        double average = (double)sum / numbers.Count;
+        int max = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum iis: {sum}");
+        Console.WriteLine($"the average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
