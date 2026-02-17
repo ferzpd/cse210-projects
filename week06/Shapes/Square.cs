485e278f31cde3776f4448c91e2a874ede42b41b
ferzpd <ferzpd25@hotmail.com>
2026-02-16 19:53:10 -0700

    Polimorfismo

---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,12 @@
+public class Square : Shape
+{
+    private double side;
+    public Square(string color, double side) : base(color)
+    {
+        this.side = side;
+    }
+    public override double GetArea()
+    {
+        return side * side;
+    }
+}
